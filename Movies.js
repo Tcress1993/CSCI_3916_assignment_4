c1b3da7e594c8734235aa1ef5e29031d4e213ea8
Tcress1993 <cresswellt54@gmail.com>
2025-04-02 23:48:38 -0600

    spelling fix

--- Movies.js
+++ Movies.js
@@ -17,7 +17,7 @@ connectDB();
 var MovieSchema = new Schema({
   title: {type: String, required: true, index: true},
   releaseDate: {type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, "Must be less than 2100"]},
-  genre: {type: String, enum: ['action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'],},
+  genre: {type: String, enum: ['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'],},
   actors: [{actorName: String, characterName: String}],
 });
 
